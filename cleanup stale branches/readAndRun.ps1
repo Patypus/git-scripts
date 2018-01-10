c8ee46bae557971abe3ccfdb1b559a9e680e4b9d
patrick howell <patrick.howell@outlook.com>
2018-01-10 20:18:50 +0000

    Added dry run ability to run script

--- cleanup stale branches/readAndRun.ps1
+++ cleanup stale branches/readAndRun.ps1
@@ -1,9 +1,19 @@
-$branchNames = Get-Content "staleBranches.txt"
+param (
+    [string]$namesFile = "staleBranches.txt",
+    [bool]$dryRun = true,
+ )
+
+$branchNames = Get-Content $namesFile
 
 foreach($branch in $branchNames) {
    $deleteCommand = "git branch -D " + $branch
    $pushCommand = "git push origin :" + $branch
-   
-   iex $deleteCommand
-   iex $pushCommand
+   if ($dryRun) {
+       Write-Host "Would run:"
+       Write-Host $deleteCommand
+       Write-Host $pushCommand
+   } else {
+        iex $deleteCommand
+        iex $pushCommand
+   }
 }
