36b70559441819f4c9ab0b148c5909946f4a52d4
patrick howell <patrick.howell@outlook.com>
2018-01-10 20:26:00 +0000

    Include passing in origin names to delete script

--- cleanup stale branches/readAndRun.ps1
+++ cleanup stale branches/readAndRun.ps1
@@ -1,5 +1,6 @@
 param (
     [string]$namesFile = "staleBranches.txt",
+    [string]$originName = "origin"
     [bool]$dryRun = true,
  )
 
@@ -7,7 +8,8 @@ $branchNames = Get-Content $namesFile
 
 foreach($branch in $branchNames) {
    $deleteCommand = "git branch -D " + $branch
-   $pushCommand = "git push origin :" + $branch
+   $pushCommand = "git push " + $originName + " :" + $branch
+   
    if ($dryRun) {
        Write-Host "Would run:"
        Write-Host $deleteCommand
